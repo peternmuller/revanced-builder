d41ffe7dc004ba6378575ed1d3e3797a1ef077a7
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-07-19 00:08:22 +0200

    build.sh: update releases text

--- build.sh
+++ build.sh
@@ -178,8 +178,8 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store\n"
+log "\nInstall [ReVanced GmsCore](https://github.com/ReVanced/GmsCore/releases/latest) for non-root YouTube and YouTube Music APKs"
+log "(Optional) Use [zygisk-detach](https://github.com/j-hc/zygisk-detach/latest) to detach root ReVanced YouTube and YouTube Music from the Play Store\n"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
