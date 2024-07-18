8f435458580c7e8daff92cf257f5c6b43476b39c
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-07-18 23:58:49 +0200

    build-termux.sh: adapt to this builder

--- build-termux.sh
+++ build-termux.sh
@@ -29,33 +29,33 @@ if [ ! -f ~/.rvmm_"$(date '+%Y%m')" ]; then
 	yes "" | pkg update -y && pkg install -y openssl git wget jq openjdk-17 zip
 	: >~/.rvmm_"$(date '+%Y%m')"
 fi
-mkdir -p /sdcard/Download/revanced-magisk-module/
+mkdir -p /sdcard/Download/revanced-builder/
 
-if [ ! -d revanced-magisk-module ]; then
-	pr "Cloning revanced-magisk-module."
-	git clone https://github.com/j-hc/revanced-magisk-module --depth 1
-	cd revanced-magisk-module
+if [ ! -d revanced-builder ]; then
+	pr "Cloning revanced-builder."
+	git clone https://github.com/peternmuller/revanced-builder --depth 1
+	cd revanced-builder
 	sed -i '/^enabled.*/d; /^\[.*\]/a enabled = false' config.toml
-	grep -q 'revanced-magisk-module' ~/.gitconfig 2>/dev/null \
-		|| git config --global --add safe.directory ~/revanced-magisk-module
+	grep -q 'revanced-builder' ~/.gitconfig 2>/dev/null \
+		|| git config --global --add safe.directory ~/revanced-builder
 else
-	cd revanced-magisk-module
-	pr "Checking for revanced-magisk-module updates"
+	cd revanced-builder
+	pr "Checking for revanced-builder updates"
 	git fetch
 	if git status | grep -q 'is behind\|fatal'; then
-		pr "revanced-magisk-module already is not synced with upstream."
-		pr "Cloning revanced-magisk-module. config.toml will be preserved."
+		pr "revanced-builder already is not synced with upstream."
+		pr "Cloning revanced-builder. config.toml will be preserved."
 		cd ..
-		cp -f revanced-magisk-module/config.toml .
-		rm -rf revanced-magisk-module
-		git clone https://github.com/j-hc/revanced-magisk-module --recurse --depth 1
-		mv -f config.toml revanced-magisk-module/config.toml
-		cd revanced-magisk-module
+		cp -f revanced-builder/config.toml .
+		rm -rf revanced-builder
+		git clone https://github.com/j-hc/revanced-builder --recurse --depth 1
+		mv -f config.toml revanced-builder/config.toml
+		cd revanced-builder
 	fi
 fi
 
-[ -f ~/storage/downloads/revanced-magisk-module/config.toml ] \
-	|| cp config.toml ~/storage/downloads/revanced-magisk-module/config.toml
+[ -f ~/storage/downloads/revanced-builder/config.toml ] \
+	|| cp config.toml ~/storage/downloads/revanced-builder/config.toml
 
 if ask "Open rvmm-config-gen to generate a config?"; then
 	am start -a android.intent.action.VIEW -d https://j-hc.github.io/rvmm-config-gen/
@@ -63,11 +63,11 @@ fi
 printf "\n"
 until
 	if ask "Open 'config.toml' to configure builds?\nAll are disabled by default, you will need to enable at first time building"; then
-		am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module/config.toml -t text/plain
+		am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-builder/config.toml -t text/plain
 	fi
 	ask "Setup is done. Do you want to start building?"
 do :; done
-cp -f ~/storage/downloads/revanced-magisk-module/config.toml config.toml
+cp -f ~/storage/downloads/revanced-builder/config.toml config.toml
 
 ./build.sh
 
@@ -78,10 +78,10 @@ for op in *; do
 		pr "glob fail"
 		exit 1
 	}
-	mv -f "${PWD}/${op}" ~/storage/downloads/revanced-magisk-module/"${op}"
+	mv -f "${PWD}/${op}" ~/storage/downloads/revanced-builder/"${op}"
 done
 
-pr "Outputs are available in /sdcard/Download/revanced-magisk-module folder"
-am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module -t resource/folder
+pr "Outputs are available in /sdcard/Download/revanced-builder folder"
+am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-builder -t resource/folder
 sleep 2
-am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-magisk-module -t resource/folder
+am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-builder -t resource/folder
