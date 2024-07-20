869a102ac2924cca3f9756b9c0218c45d8c4d813
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-07-20 20:49:21 +0200

    service.sh: adapt to this builder

--- revanced-magisk/service.sh
+++ revanced-magisk/service.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-RVPATH=$NVBASE/rvhc/${MODDIR##*/}.apk
+RVPATH=$NVBASE/rvpnm/${MODDIR##*/}.apk
 . "$MODDIR/config"
 
 until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
