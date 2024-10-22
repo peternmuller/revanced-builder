c8396e2acf91bf9bc694c0afe46090c353f4e44f
j-hc <j-hc@users.noreply.github.com>
2024-10-22 12:18:02 +0300

    module: include versionCode in version string

--- utils.sh
+++ utils.sh
@@ -598,7 +598,7 @@ MODULE_ARCH=$ma" >"$1/config"
 module_prop() {
 	echo "id=${1}
 name=${2}
-version=${3}
+version=${3} (${NEXT_VER_CODE})
 versionCode=${NEXT_VER_CODE}
 author=Peter (@peternmuller)
 description=${4}" >"${6}/module.prop"
